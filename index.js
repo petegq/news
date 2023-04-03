429c41c1ab28a243f9e89bb5b2469a7a9625a9c5
Peter Garside-Quin <petegq@gmail.com>
2023-04-03 14:14:19 +0100

    Scrape BBC

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ const cheerio = require("cheerio");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json("Hello World");
+  res.json("Welcome to the Tech News API");
+});
+
+app.get("/news", (req, res) => {
+  const bbcUrl = "www.bbc.com";
+  const topic = "/news/technology";
+  axios.get("https://" + bbcUrl + topic).then((response) => {
+    const $ = cheerio.load(response.data);
+    const news = [];
+    $(".gs-c-promo-body").each((i, el) => {
+      if ($(el).find("time").attr("datetime")) {
+        news.push({
+          source: bbcUrl,
+          title: $(el).find("h3").text(),
+          link: $(el).find("a").attr("href"),
+          datetime: $(el).find("time").attr("datetime"),
+        });
+      }
+    });
+    res.json(news);
+  });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
